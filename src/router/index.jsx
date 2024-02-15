cbbd5f125adc6e19d5b4f933f2a1267cdfbf1d8d
Mardin <mardin.ezatpana@gmail.com>
2024-02-15 14:31:16 +0330

    add aboutPage to app

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import { Route, createBrowserRouter, createRoutesFromElements } from "react-rout
 import Layout from "../layout";
 import Home from "../pages/Home/Home";
 import Shop from "../pages/Shop/Shop";
+import About from "../pages/About/About";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -9,6 +10,7 @@ export const router = createBrowserRouter(
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
           <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/about" element={<About />}></Route>
         </Route>
       </Route>
     )
