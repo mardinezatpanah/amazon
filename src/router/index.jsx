63573567fbbaf6e946234d7fe3515ea8d130697c
Mardin <mardin.ezatpana@gmail.com>
2024-02-25 21:57:53 +0330

    add contactPage to project

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,7 @@ import Layout from "../layout";
 import Home from "../pages/Home/Home";
 import Shop from "../pages/Shop/Shop";
 import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -11,6 +12,7 @@ export const router = createBrowserRouter(
           <Route index element={<Home />}></Route>
           <Route path="/shop" element={<Shop />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
         </Route>
       </Route>
     )
