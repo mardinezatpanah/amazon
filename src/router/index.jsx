0f227146edfddfaef14bc5237f5105560858cc40
Mardin <mardin.ezatpana@gmail.com>
2024-02-26 18:15:35 +0330

    add journalPage to project

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home";
 import Shop from "../pages/Shop/Shop";
 import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
+import Journal from "../pages/Journal/Journal";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -13,6 +14,7 @@ export const router = createBrowserRouter(
           <Route path="/shop" element={<Shop />}></Route>
           <Route path="/about" element={<About />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/journal" element={<Journal />}></Route>
         </Route>
       </Route>
     )
